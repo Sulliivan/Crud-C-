4dbfbca78d38ad7b3253b2a694564298154a3484
Sulliivan <64897675+Sulliivan@users.noreply.github.com>
2020-10-21 15:56:52 +0200

    Initial commit

---
+++ AvisFormationCore.WebUi/Controllers/FormationController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AvisFormationCore.WebUi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Data;
+using System.Linq;
+
+
+namespace AvisFormationCore.WebUi.Controllers
+{
+    public class FormationController : Controller
+    {
+
+
+
+        public IActionResult Index()
+        {
+                var vm = new ToutesLesFormationsViewModel();
+                vm.Message = "Ceci est un mesage du controlleur";
+                vm.Nombre = 93;
+
+                return View(vm);
+         }
+
+
+
+
+
+
+        public IActionResult ToutesLesFormations()
+         {
+            FormationMemoryRepository repository = new FormationMemoryRepository();
+            var listFormations = repository.GetAllFormations();
+
+            return View(listFormations);
+         }
+
+
+
+
+
+
+        public IActionResult DetailsFormations(string idFormation)
+        {
+            int iIdFormation = -1;
+            if(!Int32.TryParse(idFormation,out iIdFormation))
+            {
+                return RedirectToAction("ToutesLesFormations");
+            }
+
+            FormationMemoryRepository repository = new FormationMemoryRepository();
+            var formation = repository.GetFormationById(iIdFormation);
+            if(formation == null)
+            {
+                return RedirectToAction("ToutesLesFormations");
+            }
+
+            return View(formation);
+        }
+    }
+}
---
+++ Data/FormationMemoryRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Data
+{
+    public class FormationMemoryRepository
+    {
+        private List<Formation> _formations = new List<Formation>();
+        public FormationMemoryRepository()
+        {
+            _formations.Add(new Formation { Id = 1, Nom = "Creer votre site web avec ASP.NET Core", NomSeo = "asp-net-core", Description = "Grace à Blah blah Blah" });
+            _formations.Add(new Formation { Id = 2, Nom = "Creer votre site web avec PHP", NomSeo = "php", Description = "Grace à cette formation Blah blah Blah" });
+            _formations.Add(new Formation { Id = 3, Nom = "Devenez un pro du jardinage", NomSeo = "pro-jardinage", Description = "Apprenez a faire du jardinage" });
+            _formations.Add(new Formation { Id = 4, Nom = "Photo Pro", NomSeo = "photo-pro", Description = "un pro de la photo, Blah blah Blah" });
+        }
+
+        public List<Formation> GetAllFormations()
+        {
+            return _formations;
+        }
+
+        public Formation GetFormationById(int iIdFormation)
+        {
+            /*foreach (var f in _formations)
+            {
+                if (f.Id == iIdFormation)
+                    return f;
+            }*/
+
+            // return _formations.Where(f => f.Id == iIdFormation).FirstOrDefault();
+            return _formations.FirstOrDefault(f => f.Id == iIdFormation);
+
+        }
+    }
+}
