faf5fbc6132873366477370ebf6546a15a906396
Sulliivan <64897675+Sulliivan@users.noreply.github.com>
2020-10-21 16:50:56 +0200

    Ctlr Avis

---
+++ AvisFormationCore.WebUi/Controllers/AvisController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AvisFormationCore.WebUi.Controllers
+{
+    public class AvisController : Controller
+    {
+        public IActionResult LaisserUnAvis(string idFormation)
+        {
+            int iIdFormation = -1;
+            if (!Int32.TryParse(idFormation, out iIdFormation))
+            {
+                return RedirectToAction("ToutesLesFormations", "Formation");
+            }
+
+            FormationMemoryRepository repository = new FormationMemoryRepository();
+            var formation = repository.GetFormationById(iIdFormation);
+            if (formation == null)
+            {
+                return RedirectToAction("ToutesLesFormations", "Formation");
+            }
+            return View(formation);
+        }
+        public IActionResult SaveComment(string commentaire, string nom, string note)
+        {
+            return View();
+        }
+    }
+}
